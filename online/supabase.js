37a97c75ed45d0a8d16d0737863866f6436f22b7
mameeeeekko <kyopei0103@gmail.com>
2026-05-11 15:54:55 +0900

    …URLとanon keyを入れ忘れてた あほちん

--- online/supabase.js
+++ online/supabase.js
@@ -2,7 +2,7 @@
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_ANON_KEY";
+const supabaseUrl = "https://oowxivavwsgucsxahmre.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vd3hpdmF2d3NndWNzeGFobXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NzY1ODAsImV4cCI6MjA5NDA1MjU4MH0.MAIzfj7_Jb8dGrxyxOdqMODAqL5-EghFCXQJq_iL2To";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
