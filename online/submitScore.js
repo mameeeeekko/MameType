9e0c2651d9f6b3a350748975fb439c113e7d68d7
mameeeeekko <kyopei0103@gmail.com>
2026-05-11 15:49:39 +0900

    まぁ一回でうまくいくとは思ってなかったし とりあえずログ入れて確認

--- online/submitScore.js
+++ online/submitScore.js
@@ -4,7 +4,9 @@ export async function submitScore(scoreData) {
   const { error } = await supabase
     .from("scores")
     .insert([scoreData]);
-
+  
+    console.log("insert result", data, error);
+    
   if (error) {
     console.error(error);
   }
